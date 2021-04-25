a21ae9d1f1d056c17a61db7b046dfc471119ba05
Saket Verma <saket3324verma@gmail.com>
2021-04-25 19:57:58 +0530

    services completed

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -8,18 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class ServicesComponent implements OnInit {
 
   images: any[] = [];
+  baseURL: string ="https://raw.githubusercontent.com/phoenixSaket/genius/main/src/assets/Images/Services/";
 
   constructor() {
-    this.images.push({ src: "assets/Images/Services/amc.png", text: "Annual Maintenance Contract", alt: "amc" });
-    this.images.push({ src: "assets/Images/Services/cctv.png", text: "CCTV Installation", alt: "cctv" });
-    this.images.push({ src: "assets/Images/Services/cpu.png", text: "Hardware Installation/Upgradation/Configuration", alt: "hardware" });
-    this.images.push({ src: "assets/Images/Services/installation.png", text: "Assembly & Installation", alt: "installation" });
-    this.images.push({ src: "assets/Images/Services/LaptopRepair.png", text: "Laptop/Desktop Repair", alt: "laptop" });
-    this.images.push({ src: "assets/Images/Services/mainten.png", text: "Computer Maintenance", alt: "maintenance" });
-    this.images.push({ src: "assets/Images/Services/recovery.png", text: "Data Recovery & Backup", alt: "recovery" });
-    this.images.push({ src: "assets/Images/Services/software.png", text: "Software Installation", alt: "software" });
-    this.images.push({ src: "assets/Images/Services/virus.png", text: "Virus & Spyware Removal", alt: "virus" });
-    this.images.push({ src: "assets/Images/Services/Wifi.png", text: "Computer Networking & Wi-Fi", alt: "networking" });
+    this.images.push({ src: this.baseURL + "amc.png", text: "Annual Maintenance Contract", alt: "amc" });
+    this.images.push({ src: this.baseURL + "cctv.png", text: "CCTV Installation", alt: "cctv" });
+    this.images.push({ src: this.baseURL + "cpu.png", text: "Hardware Installation / Upgradation / Configuration", alt: "hardware" });
+    this.images.push({ src: this.baseURL + "Installation.png", text: "Assembly & Installation", alt: "installation" });
+    this.images.push({ src: this.baseURL + "LaptopRepair.png", text: "Laptop / Desktop Repair", alt: "laptop" });
+    this.images.push({ src: this.baseURL + "mainten.png", text: "Computer Maintenance", alt: "maintenance" });
+    this.images.push({ src: this.baseURL + "recovery.png", text: "Data Recovery & Backup", alt: "recovery" });
+    this.images.push({ src: this.baseURL + "software.png", text: "Software Installation", alt: "software" });
+    this.images.push({ src: this.baseURL + "virus.png", text: "Virus & Spyware Removal", alt: "virus" });
+    this.images.push({ src: this.baseURL + "Wifi.png", text: "Computer Networking & Wi-Fi", alt: "networking" });
   }
 
   ngOnInit(): void {
